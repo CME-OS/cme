8f7f0e736407d2d815e0bd9d42cb5d53e635c9c7
Oke Ugwu <oke.ugwu@justdevelop.it>
2014-11-12 11:44:54 +0000

    parse campaign from value into name and email

--- app/commands/QueueMessages.php
+++ app/commands/QueueMessages.php
@@ -54,156 +54,153 @@ class QueueMessages extends Command
 
     while(true)
     {
-      $result = DB::select(
-        sprintf(
-          "SELECT * FROM ranges WHERE locked_by='%s' ORDER BY created ASC LIMIT 1",
-          $instanceName
-        )
-      );
-
-      if($result)
+      do
       {
-        $queueRequest = $result[0];
-
-        //grab the campaign
-        $campaign = DB::select(
+        $result = DB::select(
           sprintf(
-            "SELECT * FROM campaigns WHERE id=%d",
-            $queueRequest->campaign_id
+            "SELECT * FROM ranges WHERE locked_by='%s' ORDER BY created ASC LIMIT 1",
+            $instanceName
           )
         );
-        $campaign = $campaign[0];
 
-        //process list in chunks
-        $listTable = 'list_' . $queueRequest->list_id;
-        //TODO check that table exists before attempting to query it
-
-        $lastId       = 0;
-        $placeHolders = null;
-        do
+        if($result)
         {
-          Log::debug("Fetching Subscribers");
-          $subscribers = DB::select(
+          $queueRequest = $result[0];
+
+          //grab the campaign
+          $campaign = DB::select(
             sprintf(
-              "SELECT * FROM %s WHERE id > %d
-               AND id BETWEEN %d AND %d
-               AND bounced=0 AND unsubscribed=0
-               LIMIT 1000",
-              $listTable,
-              $lastId,
-              $queueRequest->start,
-              $queueRequest->end
+              "SELECT * FROM campaigns WHERE id=%d",
+              $queueRequest->campaign_id
             )
           );
+          $campaign = $campaign[0];
 
-          foreach($subscribers as $subscriber)
+          //process list in chunks
+          $listTable = 'list_' . $queueRequest->list_id;
+          //TODO check that table exists before attempting to query it
+
+          $lastId       = 0;
+          $placeHolders = null;
+          do
           {
-            if($placeHolders == null)
+            Log::debug("Fetching Subscribers");
+            $subscribers = DB::select(
+              sprintf(
+                "SELECT * FROM %s WHERE id > %d
+                 AND id BETWEEN %d AND %d
+                 AND bounced=0 AND unsubscribed=0
+                 LIMIT 1000",
+                $listTable,
+                $lastId,
+                $queueRequest->start,
+                $queueRequest->end
+              )
+            );
+
+            foreach($subscribers as $subscriber)
             {
-              Log::debug("Building placeholders");
-              $columns = array_keys((array)$subscriber);
-              foreach($columns as $c)
+              if($placeHolders == null)
               {
-                $placeHolders[$c] = "[$c]";
+                Log::debug("Building placeholders");
+                $columns = array_keys((array)$subscriber);
+                foreach($columns as $c)
+                {
+                  $placeHolders[$c] = "[$c]";
+                }
+                //add brand attributes as placeholders too
+                $result  = DB::select(
+                  sprintf(
+                    "SELECT * FROM brands WHERE id=%d",
+                    $campaign->brand_id
+                  )
+                );
+                $brand   = $result[0];
+                $columns = array_keys((array)$brand);
+                foreach($columns as $c)
+                {
+                  $placeHolders[$c] = "[$c]";
+                }
               }
-              //add brand attributes as placeholders too
-              $result  = DB::select(
-                sprintf(
-                  "SELECT * FROM brands WHERE id=%d",
-                  $campaign->brand_id
-                )
-              );
-              $brand   = $result[0];
-              $columns = array_keys((array)$brand);
-              foreach($columns as $c)
+
+              //parse and compile message (replacing placeholders if any)
+              Log::debug("Parsing and compiling messages");
+              $html = $campaign->html_content;
+              $text = $campaign->text_content;
+              foreach($placeHolders as $prop => $placeHolder)
               {
-                $placeHolders[$c] = "[$c]";
+                $replace = false;
+                if(property_exists($subscriber, $prop))
+                {
+                  $replace = $subscriber->$prop;
+                }
+                elseif(property_exists($brand, $prop))
+                {
+                  $replace = $brand->$prop;
+                }
+
+                $html = str_replace($placeHolder, $replace, $html);
+                $text = str_replace($placeHolder, $replace, $text);
               }
+
+              //append pixel to html content, so we can track opens
+              $domain   = Config::get('app.domain');
+              $pixelUrl = "http://" . $domain . "/trackOpen/" . $campaign->id
+                . "_" . $campaign->list_id . "_" . $subscriber->id;
+              $html .= '<img src="' . $pixelUrl
+                . '" style="display:none;" height="1" width="1" />';
+
+              list($fromName, $fromEmail) = explode(' <', $campaign->from);
+
+              //write to message queue
+              $message = [
+                'subject'       => $campaign->subject,
+                'from_name'     => $fromName,
+                'from_email'    => trim($fromEmail, '><'),
+                'to'            => $subscriber->email,
+                'html_content'  => $html,
+                'text_content'  => $text,
+                'subscriber_id' => $subscriber->id,
+                'list_id'       => $queueRequest->list_id,
+                'brand_id'      => $campaign->brand_id,
+                'campaign_id'   => $campaign->id,
+                'send_time'     => $campaign->send_time,
+                'send_priority' => $campaign->send_priority
+              ];
+              Log::debug("Queuing message for " . $subscriber->email);
+              DB::table('message_queue')->insert($message);
+
+              $lastId = $subscriber->id;
             }
-
-            //parse and compile message (replacing placeholders if any)
-            Log::debug("Parsing and compiling messages");
-            $html = $campaign->html_content;
-            $text = $campaign->text_content;
-            foreach($placeHolders as $prop => $placeHolder)
-            {
-              $replace = false;
-              if(property_exists($subscriber, $prop))
-              {
-                $replace = $subscriber->$prop;
-              }
-              elseif(property_exists($brand, $prop))
-              {
-                $replace = $brand->$prop;
-              }
-
-              $html = str_replace($placeHolder, $replace, $html);
-              $text = str_replace($placeHolder, $replace, $text);
-            }
-
-            //append pixel to html content, so we can track opens
-            $domain   = Config::get('app.domain');
-            $pixelUrl = "http://" . $domain . "/trackOpen/" . $campaign->id
-              . "_" . $campaign->list_id . "_" . $subscriber->id;
-            $html .= '<img src="' . $pixelUrl
-              . '" style="display:none;" height="1" width="1" />';
-
-            //write to message queue
-            $message = [
-              'subject'       => $campaign->subject,
-              'from'          => $campaign->from,
-              'to'            => $subscriber->email,
-              'html_content'  => $html,
-              'text_content'  => $text,
-              'subscriber_id' => $subscriber->id,
-              'list_id'       => $queueRequest->list_id,
-              'brand_id'      => $campaign->brand_id,
-              'campaign_id'   => $campaign->id,
-              'send_time'     => $campaign->send_time,
-              'send_priority' => $campaign->send_priority
-            ];
-            Log::debug("Queuing message for " . $subscriber->email);
-            DB::table('message_queue')->insert($message);
-
-            //add to campaign events table
-            $event = [
-              'campaign_id'   => $campaign->id,
-              'list_id'       => $queueRequest->list_id,
-              'subscriber_id' => $subscriber->id,
-              'event_type'    => 'queued',
-              'time'          => time()
-            ];
-            DB::table('campaign_events')->insert($event);
-
-            $lastId = $subscriber->id;
           }
-        }
-        while($subscribers);
+          while($subscribers);
 
-        DB::table('ranges')
-          ->where(
-            [
+          DB::table('ranges')
+            ->where(
+              [
               'list_id'     => $queueRequest->list_id,
               'campaign_id' => $queueRequest->campaign_id,
               'start'       => $queueRequest->start
-            ]
-          )
-          ->delete();
-      }
-      else
-      {
-        //lock a row
-        $lockedARow = DB::update(
-          "UPDATE ranges SET locked_by=?
-          WHERE locked_by IS NULL ORDER BY created ASC LIMIT 1",
-          [$instanceName]
-        );
-        if(!$lockedARow)
+              ]
+            )
+            ->delete();
+        }
+        else
         {
-          $this->info("sleeping for a bit");
-          sleep(5);
+          //lock a row
+          $lockedARow = DB::update(
+            "UPDATE ranges SET locked_by=?
+            WHERE locked_by IS NULL ORDER BY created ASC LIMIT 1",
+            [$instanceName]
+          );
+          if(!$lockedARow)
+          {
+            $this->info("sleeping for a bit");
+            sleep(5);
+          }
         }
       }
+      while($result);
     }
   }
 

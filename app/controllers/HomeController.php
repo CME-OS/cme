3d3ebd8d392ae8cd6c3bd223d78034911ef26c6b
Oke Ugwu <oke.ugwu@justdevelop.it>
2014-11-14 16:06:40 +0000

    show last24 hours and last 7 days for each campaign

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,42 +4,70 @@ class HomeController extends BaseController
 {
   public function index()
   {
-    $reportType = [7, 30];
     $eventTypes = [
-      'failed',
       'queued',
       'sent',
       'opened',
       'clicked',
+      'failed',
       'bounced',
       'unsubscribed'
     ];
 
-    foreach($reportType as $period)
-    {
-      $time = strtotime("-$period days");
-      $events       = DB::select(
-        $x = "SELECT * FROM campaign_events WHERE time >= $time"
-      );
+    $time = strtotime("-7 days midnight");
+    $endTime   = strtotime(date('Y-m-d 23:59:59'));
+    $events    = DB::select(
+      $x = "SELECT * FROM campaign_events WHERE time >= $time"
+    );
 
-      $stats[$period] = [];
-      foreach($events as $event)
+    $dStats = []; //daily stats
+    $hStats = []; //hourly stats
+    foreach($events as $event)
+    {
+      if(!isset($stats[$event->campaign_id]))
       {
-        if(!isset($stats[$period][$event->campaign_id]))
+        //build blank slate for daily stats
+        $startTime = $time;
+        for($s = $startTime; $s <= $endTime; $s += 86400)
         {
+          $day = date('Y-m-d', $s);
           foreach($eventTypes as $type)
           {
-            $stats[$period][$event->campaign_id][$type] = 0;
+            $dStats[$event->campaign_id][$day][$type] = 0;
           }
         }
 
-        $stats[$period][$event->campaign_id][$event->event_type]++;
+        //build blank slate for hourly stats
+        $startTime = strtotime(date('Y-m-d 00:00:00'));
+        for($s = $startTime; $s <= $endTime; $s += 3600)
+        {
+          $hour = date('Y-m-d H:00:00', $s);
+          foreach($eventTypes as $type)
+          {
+            $hStats[$event->campaign_id][$hour][$type] = 0;
+          }
+        }
+      }
+      else
+      {
+        $day = date('Y-m-d', $event->time);
+        if(isset($dStats[$event->campaign_id][$day]))
+        {
+          $dStats[$event->campaign_id][$day][$event->event_type]++;
+        }
+
+        $hour = date('Y-m-d H:00:00', $event->time);
+        if(isset($hStats[$event->campaign_id][$hour]))
+        {
+          $hStats[$event->campaign_id][$hour][$event->event_type]++;
+        }
       }
     }
 
-
-    $campaignLookUp = DB::table('campaigns')->lists('subject', 'id');
-    $data['stats'] = $stats;
+    $campaignLookUp         = DB::table('campaigns')->lists('subject', 'id');
+    $data['eventTypes']     = $eventTypes;
+    $data['dStats']          = $dStats;
+    $data['hStats']          = $hStats;
     $data['campaignLookUp'] = $campaignLookUp;
 
     return View::make('home', $data);

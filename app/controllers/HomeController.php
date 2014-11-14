c4075dcd5dd6fba82a87a7abad4091afc2d5193b
Oke Ugwu <oke.ugwu@justdevelop.it>
2014-11-14 15:01:41 +0000

    display stats on homepage

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,6 +4,44 @@ class HomeController extends BaseController
 {
   public function index()
   {
-    return View::make('home');
+    $reportType = [7, 30];
+    $eventTypes = [
+      'failed',
+      'queued',
+      'sent',
+      'opened',
+      'clicked',
+      'bounced',
+      'unsubscribed'
+    ];
+
+    foreach($reportType as $period)
+    {
+      $time = strtotime("-$period days");
+      $events       = DB::select(
+        $x = "SELECT * FROM campaign_events WHERE time >= $time"
+      );
+
+      $stats[$period] = [];
+      foreach($events as $event)
+      {
+        if(!isset($stats[$period][$event->campaign_id]))
+        {
+          foreach($eventTypes as $type)
+          {
+            $stats[$period][$event->campaign_id][$type] = 0;
+          }
+        }
+
+        $stats[$period][$event->campaign_id][$event->event_type]++;
+      }
+    }
+
+
+    $campaignLookUp = DB::table('campaigns')->lists('subject', 'id');
+    $data['stats'] = $stats;
+    $data['campaignLookUp'] = $campaignLookUp;
+
+    return View::make('home', $data);
   }
 }
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,8 +3,37 @@
 <div>
   <h1>Welcome to CME</h1>
 
-  <p class="well">CME stands for Campaign Made Easy. CME allows you to manage and schedule campaigns across all your brands.
-  CME is designed for high volume campaigns and is very robust</p>
+  <?php if (!$stats): ?>
+    <p class="well">CME stands for Campaign Made Easy. CME allows you to manage
+                    and schedule campaigns across all your brands.
+                    CME is designed for high volume campaigns and is very
+                    robust</p>
+  <?php else: ?>
+  <div class="row">
+    <div class="col-md-6">
+      <h2>Last 7 Days</h2>
+      <?php foreach ($stats[7] as $campaignId => $eventStats): ?>
+      <div class="well">
+        <h3><?= $campaignLookUp[$campaignId] ?></h3>
+        <?php foreach($eventStats as $event => $count): ?>
+          <p class="badge"><?= $event ?>: <?= $count ?></p>
+        <?php endforeach; ?>
+      </div>
+      <?php endforeach; ?>
+    </div>
+    <div class="col-md-6">
+      <h2>Last 30 Days</h2>
+      <?php foreach ($stats[30] as $campaignId => $eventStats): ?>
+        <div class="well">
+          <h3><?= $campaignLookUp[$campaignId] ?></h3>
+          <?php foreach($eventStats as $event => $count): ?>
+            <p class="badge"><?= $event ?>: <?= $count ?></p>
+          <?php endforeach; ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+  <?php endif; ?>
 
 </div>
 @stop

88a01d3a3cddc281e4824410e4f7d6149cbc4204
Oke Ugwu <oke.ugwu@justdevelop.it>
2015-02-02 11:38:35 +0000

    improved home page to show last 5 campaigns

--- app/Cme/Web/Controllers/HomeController.php
+++ app/Cme/Web/Controllers/HomeController.php
@@ -17,53 +17,55 @@ class HomeController extends BaseController
       'unsubscribed'
     ];
 
-    $time    = strtotime("-7 days midnight");
-    $endTime = strtotime(date('Y-m-d 23:59:59'));
-    $events  = DB::select(
-      "SELECT * FROM campaign_events WHERE time >= $time"
-    );
-
     $dStats = []; //daily stats
     $hStats = []; //hourly stats
-    foreach($events as $event)
+    $campaigns         = DB::select('SELECT * FROM campaigns ORDER BY send_time DESC LIMIT 5');
+    foreach($campaigns as $campaign)
     {
-      if(!isset($dStats[$event->campaign_id]))
+      $endTime = strtotime(date('Y-m-d 23:59:59'));
+      $events  = DB::select(
+        "SELECT * FROM campaign_events WHERE campaign_id = $campaign->id"
+      );
+      foreach($events as $event)
       {
-        //build blank slate for daily stats
-        $startTime = $time;
-        for($s = $startTime; $s <= $endTime; $s += 86400)
+        if(!isset($dStats[$event->campaign_id]))
         {
-          $day = date('Y-m-d', $s);
-          foreach($eventTypes as $type)
+          //build blank slate for daily stats
+          $startTime = $campaign->send_time;
+          for($s = $startTime; $s <= $endTime; $s += 86400)
           {
-            $dStats[$event->campaign_id][$day][$type] = 0;
+            $day = date('Y-m-d', $s);
+            foreach($eventTypes as $type)
+            {
+              $dStats[$event->campaign_id][$day][$type] = 0;
+            }
           }
         }
-      }
-      if(!isset($hStats[$event->campaign_id]))
-      {
-        //build blank slate for hourly stats
-        $startTime = strtotime(date('Y-m-d 00:00:00'));
-        for($s = $startTime; $s <= $endTime; $s += 3600)
+        if(!isset($hStats[$event->campaign_id]))
         {
-          $hour = date('Y-m-d H:00:00', $s);
-          foreach($eventTypes as $type)
+          //build blank slate for hourly stats
+          $startTime = strtotime(date('Y-m-d 00:00:00'));
+          for($s = $startTime; $s <= $endTime; $s += 3600)
           {
-            $hStats[$event->campaign_id][$hour][$type] = 0;
+            $hour = date('Y-m-d H:00:00', $s);
+            foreach($eventTypes as $type)
+            {
+              $hStats[$event->campaign_id][$hour][$type] = 0;
+            }
           }
         }
-      }
 
-      $day = date('Y-m-d', $event->time);
-      if(isset($dStats[$event->campaign_id][$day]))
-      {
-        $dStats[$event->campaign_id][$day][$event->event_type]++;
-      }
+        $day = date('Y-m-d', $event->time);
+        if(isset($dStats[$event->campaign_id][$day]))
+        {
+          $dStats[$event->campaign_id][$day][$event->event_type]++;
+        }
 
-      $hour = date('Y-m-d H:00:00', $event->time);
-      if(isset($hStats[$event->campaign_id][$hour]))
-      {
-        $hStats[$event->campaign_id][$hour][$event->event_type]++;
+        $hour = date('Y-m-d H:00:00', $event->time);
+        if(isset($hStats[$event->campaign_id][$hour]))
+        {
+          $hStats[$event->campaign_id][$hour][$event->event_type]++;
+        }
       }
     }
 

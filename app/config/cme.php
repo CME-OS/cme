6232267712f77bf2608e197e094d23b1c5457f6f
fobilow-thinkpad <fobilow@gmail.com>
2015-02-21 07:38:54 +0000

    removed bouce_url from config as it not needed

--- app/config/cme.php
+++ app/config/cme.php
@@ -5,5 +5,4 @@ return array(
   'aws_key'         => $_ENV['aws_key'],
   'aws_secret'      => $_ENV['aws_secret'],
   'aws_region'      => $_ENV['aws_region'],
-  'sqs_bounce_queue' => $_ENV['sqs_bounce_queue'],
 );

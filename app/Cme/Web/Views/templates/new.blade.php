f31d435985f273403772a1d826b6acce6be78e71
Okechukwu <fobilow"gmail.com>
2015-06-02 22:34:45 +0100

    form validation - retain template name

--- app/Cme/Web/Views/templates/new.blade.php
+++ app/Cme/Web/Views/templates/new.blade.php
@@ -10,7 +10,7 @@
       <h2>Create a Template</h2>
       <div class="form-group <?= isset($errors['name'])? 'has-error has-feedback': '' ?>">
         <label for="template-name">Name <span class="text-danger" style="font-size: 11px; font-style: italic;"><?= isset($errors['name'])? ' - '.$errors['name']->message: '' ?></span></label>
-        <input type="text" name="name" class="form-control" id="template-name" placeholder="Name">
+        <input type="text" name="name" class="form-control" id="template-name" placeholder="Name" value="<?= isset($input['name'])? $input['name'] : $template->name ?>">
         <span class="glyphicon glyphicon-remove form-control-feedback <?= isset($errors['name'])? '': 'hidden' ?>" aria-hidden="true"></span>
       </div>
       <div class="form-group <?= isset($errors['content'])? 'has-error has-feedback': '' ?>">

f7fb7f0b3a47c2aedabb06a8c3f30d1daa7fd04b
Oke Ugwu <oke.ugwu@justdevelop.it>
2015-06-29 11:07:15 +0100

    implemented a proper sticky footer

--- app/Cme/Web/Views/layouts/default.blade.php
+++ app/Cme/Web/Views/layouts/default.blade.php
@@ -5,11 +5,12 @@
 </head>
 <body>
 @include('partials.header')
-<div class="content container-fluid" style="margin-bottom: 50px;">
+<div class="content container-fluid">
   <?php if($state->showWizardButton):?>
   <div class="pull-right" style="margin-top:30px;"><a href="/" class="btn btn-danger">Return to Getting Started</a></div>
   <?php endif; ?>
   @yield('content')
+    <div class="push"></div>
 </div>
 @include('partials.footer')
 </body>
